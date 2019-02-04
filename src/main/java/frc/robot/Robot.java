9c8014a6cf5ac57acf6937ee0655f58a0737b43e
commodores <darren.hagler@4cacademy.org>
2019-02-03 20:32:31 -0500

    allgreen

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.*;
-import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
