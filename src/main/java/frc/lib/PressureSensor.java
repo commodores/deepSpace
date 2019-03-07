a2fa8bb897154956f5f30c6f723a45838bca6532
commodores <Mc1083P0>
2019-03-07 15:00:59 -0500

    yes

---
+++ src/main/java/frc/lib/PressureSensor.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.lib;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+/**
+ * Add your docs here.
+ */
+public class PressureSensor {
+
+    private final AnalogInput mAnalogInput;
+
+    public PressureSensor(int analogInputNumber) {
+        mAnalogInput = new AnalogInput(analogInputNumber);
+    }
+
+    public double getAirPressurePsi() {
+        // taken from the datasheet
+        return 250.0 * mAnalogInput.getVoltage() / 5.0 - 25.0;
+    }
+
+}
